a7f6dadf83085b0732d901b7d0a33178d38cd3ae
Peter <pkabai@csumb.edu>
2018-12-11 23:11:16 -0800

    Adds feedback to show how many images have nbeen processed

--- imagePreprocessing.bash
+++ imagePreprocessing.bash
@@ -3,13 +3,13 @@
 # defines dir to look in for files 
 files=./images/*.jpg
 
-# sets limits, images with hours between these will be kept
+# sets range, images with hours between these will be kept
 lowLimitHours=5
-highLimitHours=22
+highLimitHours=21
 widthToResize=480
 
 # user confirmation before deleting images
-echo "This will delete all images before $lowLimitHours:00 and after $highLimitHours:00"
+echo "This will delete all images before $lowLimitHours:00 and after `expr $highLimitHours-1`:59"
 read -p "Continue anyway (y/n)? " cont
 if [ "$cont" != "y" ]
 then
@@ -18,19 +18,19 @@ then
 fi
 echo "Ok, continuing..."
 
-# gets the number of characters in the filename and path
-set -- ${files}
-numChar=${#1}
-
 # counts the number of files in the directory
 numberOfFiles=0
 numberDeleted=0
 numberResized=0
 for file in $files
 do
-	hour=${file:(numChar-9):2}
+	# gets the hour of the image
+	hour="`echo $file | tail -c 10 | head -c 2`"
+	
+	# if the hour is outside the specified hour range
 	if [ $hour -gt $highLimitHours ] || [ $hour -lt $lowLimitHours ]
 	then
+		# removes the file
 		rm "$file"
 		numberDeleted=`expr $numberDeleted + 1`
 	else
@@ -38,9 +38,14 @@ do
 		width="`echo $fileInfo | tail -c 18 | head -c 3`"
 		if [ $width != $widthToResize ]
 		then
-			sips -Z $widthToResize "$file"
+			# resizes the image, may only work on mac
+			sips -Z $widthToResize "$file" &>/dev/null &disown
 			numberResized=`expr $numberResized + 1`
 		fi
+	fi
+	if [ $(($numberOfFiles%100)) == 0 ] && [ $numberOfFiles != 0 ]
+	then
+		echo $numberOfFiles" images processed..."
 	fi
 	numberOfFiles=`expr $numberOfFiles + 1`
 done
