96405d6db51f10b7be302c30800fa83b17471bd8
Peter <pkabai@csumb.edu>
2018-12-11 18:36:17 -0800

    Adds user confirmation, now deletes the nighttime images

--- preprocessing.bash
+++ preprocessing.bash
@@ -3,21 +3,37 @@
 # defines dir to look in for files 
 files=./images/*.jpg
 
+# sets limits, images in hours between these will be kept
+lowLimitHours=5
+highLimitHours=22
+
+# user confirmation
+echo "This will delete all images before $lowLimitHours:00 and after $highLimitHours:00"
+read -p "Continue anyway (y/n)? " cont
+if [ "$cont" != "y" ]
+then
+  echo "Ok, exiting."
+  exit
+fi
+echo "Ok, continuing..."
+
 # number of characters in the filename and path
 set -- ${files}
 numChar=${#1}
-echo $numChar
 
 # counts the number of files in the directory
 numberOfFiles=0
 numberDeleted=0
 for file in $files
 do
-	echo "Processing ${file}"
 	hour=${file:(numChar-9):2}
-	if [ $hour -gt 22 ] || [ $hour -lt 4 ]; then
-		echo "Nighttime"
+	if [ $hour -gt $highLimitHours ] || [ $hour -lt $lowLimitHours ]
+	then
+		rm "$file"
+		numberDeleted=`expr $numberDeleted + 1`
 	fi
 	numberOfFiles=`expr $numberOfFiles + 1`
 done
 
+echo "Done!"
+echo $numberDeleted" of "$numberOfFiles" images deleted."
